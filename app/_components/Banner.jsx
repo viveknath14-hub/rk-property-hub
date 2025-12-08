b631b48f83932f4d4653d8dbffe7fa1b8f12d325
vivek <viveknath14gmail.com>
2025-12-09 01:38:38 +0530

    Made changes to navbar and created a new file banner

---
+++ app/_components/Banner.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div className='con'></div>
+  )
+}
+
+export default Banner
--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -24,7 +24,8 @@ import {
 import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
 import Link from 'next/link'
 import Image from 'next/image'
-import { ArrowRight } from 'lucide-react'
+import { ArrowRight, MapPinHouse, Smile, User2Icon, UserRoundSearchIcon } from 'lucide-react'
+import { Button } from '@/components/ui/button'
 
 const products = [
   { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
@@ -42,17 +43,16 @@ export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <header className="bg-white" >
-      <nav aria-label="Global" 
-      className="mx-auto flex container items-center justify-between p-6 lg:px-8 fl">
-        <div className="flex lg:flex-1 ">
-          <Link href="#" >
-            <Image 
-            src="/rkPropertyHub.png" 
-            alt="RK Property Hub logo" 
-            width={160} 
-            height={80} 
-            priority
+    <header className="bg-white">
+      <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+        <div className="flex lg:flex-1">
+          <Link href="#" className="-m-1.5 p-1.5">
+            <Image
+              src="/rkPropertyHub.png"
+              alt="RK Property Hub logo"
+              width={190}
+              height={80}
+              priority
             />
           </Link>
         </div>
@@ -67,21 +67,29 @@ export default function Header() {
           </button>
         </div>
         <PopoverGroup className="hidden lg:flex lg:gap-x-12">
-         
+
           <Link href="#" className="text-lg font-normal tracking-wide text-gray-900 ">
-            Features
+            Home
           </Link>
           <Link href="#" className="text-lg font-normal tracking-wide text-gray-900">
-            Marketplace
+            About us
           </Link>
           <Link href="#" className="text-lg font-normal tracking-wide text-gray-900">
-            Company
+            Services
+          </Link>
+          <Link href="#" className="text-lg font-normal tracking-wide text-gray-900">
+            Facilities
+          </Link>
+          <Link href="#" className="text-lg font-normal tracking-wide text-gray-900">
+            Blogs
           </Link>
         </PopoverGroup>
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <Link href="#" className="text-lg font-normal tracking-wide text-gray-900 flex items-center gap-2">
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end gap-2">
+          {/* <Link href="#" className="text-lg font-normal tracking-wide text-gray-900 flex items-center gap-2">
             Log in <ArrowRight size={20} />
-          </Link>
+          </Link> */}
+          <Button><User2Icon size={20} />Contact us</Button>
+          <Button variant="secondary"><MapPinHouse size={20} />Visit now</Button>
         </div>
       </nav>
       <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
@@ -89,12 +97,12 @@ export default function Header() {
         <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <Link href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
+              <span className="sr-only">RK Property Hub logo</span>
               <Image
-                alt="Your Company"
-                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-                width={32}
-                height={32}
+                alt="RK Property Hub logo"
+                src="/rkPropertyHub.png"
+                width={160}
+                height={80}
                 className="h-8 w-auto"
               />
             </Link>
@@ -110,50 +118,14 @@ export default function Header() {
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
-                <Disclosure as="div" className="-mx-3">
-                  <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pr-3.5 pl-3 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
-                    Product
-                    <ChevronDownIcon aria-hidden="true" className="size-5 flex-none group-data-open:rotate-180" />
-                  </DisclosureButton>
-                  <DisclosurePanel className="mt-2 space-y-2">
-                    {[...products, ...callsToAction].map((item) => (
-                      <DisclosureButton
-                        key={item.name}
-                        as="a"
-                        href={item.href}
-                        className="block rounded-lg py-2 pr-3 pl-6 text-sm/7 font-semibold text-gray-900 hover:bg-gray-50"
-                      >
-                        {item.name}
-                      </DisclosureButton>
-                    ))}
-                  </DisclosurePanel>
-                </Disclosure>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                >
-                  Features
-                </a>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                >
-                  Marketplace
-                </a>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                >
-                  Company
-                </a>
+                <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Home</a>
+                <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">About us</a>
+                <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Services</a>
+                <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Facilities</a>
+                <a href="#" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Blogs</a>
               </div>
               <div className="py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                >
-                  Log in
-                </a>
+                <a href="#" className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">Log in</a>
               </div>
             </div>
           </div>
