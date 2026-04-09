2e8e5b8c514ab4319b17a076ff0df6eb70d0106c
vivek <viveknath14gmail.com>
2026-04-09 13:34:53 +0530

    menifest added

---
+++ app/manifest.js
@@ -0,0 +1,23 @@
+export default function manifest() {
+  return {
+    name: 'RK Property Hub',
+    short_name: 'RKPH',
+    description: 'Trusted real estate platform in Delhi NCR',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#000000',
+    icons: [
+      {
+        src: '/icon.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/icon.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  }
+}
